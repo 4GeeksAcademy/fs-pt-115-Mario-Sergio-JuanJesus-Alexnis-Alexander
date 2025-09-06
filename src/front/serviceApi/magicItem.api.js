a0b5c866a2266884a6c404dc3969ee634baca00b
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-09-06 17:31:22 +0000

    feat: crear la llamada y para crear un item magico y comprobar con postman que funciona bien

---
+++ src/front/serviceApi/magicItem.api.js
@@ -0,0 +1,35 @@
+const urlApi = import.meta.env.VITE_BACKEND_URL;
+
+export const createMagicItem = async (newMagicItem) => {
+
+    try {
+        const response = await fetch(`${urlApi}/api/user/magics-items`, {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            },
+            body: JSON.stringify(newMagicItem)
+        })
+
+        const data = await response.json()
+
+        if (!response.ok) {
+            return {
+                success: false,
+                error: data.error || 'Error al crear Item Magico'
+            }
+        }
+
+        return {
+        success: true,
+        data: data,
+        token: data.token
+    }
+
+    } catch (error) {
+        return {
+            success: false, 
+            error: error.message }
+    }
+}
