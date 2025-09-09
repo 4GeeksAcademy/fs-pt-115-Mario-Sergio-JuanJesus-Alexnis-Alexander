a3b58a3620ad66aae188b774886d7c59c38ac8ce
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-09-09 14:17:39 +0000

    feat: hacer llamadas de api de los articulos magicos, ahora hacer merge y ya probar todo el conjunto para verificar errores y solucionarlos

--- src/front/serviceApi/magicItem.api.js
+++ src/front/serviceApi/magicItem.api.js
@@ -1,3 +1,5 @@
+import { data } from "@remix-run/router";
+
 const urlApi = import.meta.env.VITE_BACKEND_URL;
 
 export const createMagicItem = async (newMagicItem) => {
@@ -24,7 +26,7 @@ export const createMagicItem = async (newMagicItem) => {
         return {
         success: true,
         data: data,
-        token: data.token
+        msg: data.msg
     }
 
     } catch (error) {
@@ -34,21 +36,130 @@ export const createMagicItem = async (newMagicItem) => {
     }
 }
 
-export const showMagicItem = async () => {
+export const showMagicItem = async (itemId) => {
     try {
-        const response = await fetch(`${urlApi}/user/magics-items`, {
+        const response = await fetch(`${urlApi}/user/magics-items/${itemId}`, {
             method: 'GET',
             headers: {
-                'Content-Type': 'application/json',
-                'Autorizathion': `Bearer ${localStorage.getItem('token')}`
+                'Autorization': `Bearer ${localStorage.getItem('token')}`
             }
         })
 
         const data = await response.json()
 
-        
+        if (!response.ok) {
+            return {
+                success: false,
+                error: data.error || 'Error al mostrar el articulo magico'
+            }
+        }
+
+        return {
+            success: true,
+            data: data,
+            msg: data.msg
+        }
 
     } catch (error) {
-        
+        return {
+            success: false, 
+            error: error.message }
+    }
+}
+
+export const updateMagicItem = async (updateItem, itemId) => {
+    try {
+        const response = await fetch(`${urlApi}/user/magics-items/${itemId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Autorization': `Bearer ${localStorage.getItem('token')}`
+            },
+            body: JSON.stringify(updateItem)
+    })
+
+    const data = await response.json()
+
+    if (!response.ok) {
+        return {
+            success: false,
+            error: data.error
+        }
+    }
+
+    return {
+        success: true,
+        data: data,
+        msg: data.msg
+    }
+
+    } catch (error) {
+        return {
+            success: false,
+            error: error.message
+        }
+    }
+}
+
+export const deleteMagicItem = async (itemId) => {
+
+    try {
+        const response = await fetch(`${urlApi}/user/magics-items/${itemId}`, {
+        method: 'DELETE',
+        headers: {
+                'Autorization': `Bearer ${localStorage.getItem('token')}`
+            }
+    })
+
+    const data = await response.json()
+
+    if (!response.ok) {
+        return {
+            success: false,
+            error: data.error
+        }
+    }
+
+    return {
+        success: true,
+        msg: data.msg
+    }
+
+    } catch (error) {
+        return {
+            success: false,
+            error: error.message
+        }
+    }
+    
+}
+
+export const showAllMMagicItems = async () => {
+    try {
+        const response = await fetch(`${urlApi}/user/magics-items`, {
+            method: 'GET',
+            headers: {
+                'Autorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+
+        const data = await response.json()
+
+        if (!response.ok) {
+            return {
+                success: false,
+                error: data.error
+            }
+        }
+
+        return {
+            success: true,
+            data: data
+        }
+    } catch (error) {
+        return {
+            success: false,
+            error: error.message
+        }
     }
 }
