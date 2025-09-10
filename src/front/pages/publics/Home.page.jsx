19b9554829e37ac0ef15c9b2acef04d7a2cad8f8
Alexnis-Herrera <alexnisherrerag@gmail.com>
2025-09-10 17:28:29 +0000

    backgroundliato

--- src/front/pages/publics/Home.page.jsx
+++ src/front/pages/publics/Home.page.jsx
@@ -1,13 +1,13 @@
 
 import { FormMagicItems } from "../../components/FormMagicsItems";
 import { CollectionDropdown } from "../../components/CollectionDropdown";
+import { FormularioBackground } from "../../components/FormularioBackground";
 
 export const Home = () => {
 
 	return (
 		<>
 			<h1 className="text-center mt-5">D&D MASTERS OF INFINITY</h1>
-			
 		</>
 		
 	);
