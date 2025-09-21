7b57d39282add70358f70ce70330f5d16d339904
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-09-21 18:49:18 +0000

    feat: crenado logica del formulario para activar o desactivar inputs

--- src/front/components/FormMagicsItems.jsx
+++ src/front/components/FormMagicsItems.jsx
@@ -1,78 +1,37 @@
 import { useState } from "react";
 import { createMagicItem } from "../serviceApi/magicItem.api";
 import { useNavigate } from "react-router-dom";
+import { useDisabledFields } from "../hooks/useDisabledFields";
+import { initialDisabled, rules } from "../rules-forms/magicsItems.rules";
 
 export const FormMagicItems = () => {
+  
   const [input, setInputs] = useState({});
   const [error, setError] = useState(null);
-  const [isDisabled, setIsDisabled] = useState({
-    base_armor: true,
-    base_weapon: true,
-    dex_bonus: true,
-    str_requirement: true,
-    stealth_check: true,
-    magic_item_type: true,
-    attunement_description: true,
-  });
+  const { disabledFields, updateDisabledFields } = useDisabledFields(
+    rules,
+    initialDisabled
+  );
   const navigate = useNavigate();
-
+  
   const handleOnChange = (e) => {
-    const { name, value } = e.target;
-    const formMagicsItems = { ...input, [name]: value };
-    setInputs(formMagicsItems);
+    const { name, value, checked, type } = e.target;
 
-    if (name === "base_item_type") {
-      if (value === "Item") {
-        setIsDisabled({
-          base_armor: true,
-          base_weapon: true,
-          dex_bonus: true,
-          str_requirement: true,
-          stealth_check: true,
-          magic_item_type: false,
-        });
-      } else if (value === "Armor") {
-        setIsDisabled({
-          ...isDisabled,
-          base_armor: false,
-          base_weapon: true,
-          dex_bonus: false,
-          str_requirement: false,
-          stealth_check: false,
-          magic_item_type: true,
-        });
-      } else if (value === "Weapon") {
-        setIsDisabled({
-          ...isDisabled,
-          base_weapon: false,
-          base_armor: true,
-          dex_bonus: true,
-          str_requirement: true,
-          stealth_check: true,
-          magic_item_type: true,
-        });
-      }
-    } else if (name === "requires_attunement") {
-      const isChecked = e.target.checked;
-      setIsDisabled({
-        ...isDisabled,
-        attunement_description: !isChecked,
-      });
-    }
+    const inputValue = type === "checkbox" ? checked : value;
+
+    setInputs({ ...input, [name]: inputValue });
+    updateDisabledFields(name, inputValue);
+    
   };
-
   const handleOnSubmit = async (e) => {
     e.preventDefault();
-
     const dataMagicItem = await createMagicItem(input);
-
     if (!dataMagicItem.success) {
       return setError(dataMagicItem?.error || "Creación fallida");
     } else {
       setInputs({});
       setError(null);
     }
-
     navigate("/user/magics-items");
   };
 
@@ -117,9 +76,13 @@ export const FormMagicItems = () => {
           >
             <option value="">---</option>
             <option value={"Common"}>Common</option>
+            <option value={"Uncommon"}>Uncommon</option>
             <option value={"Rare"}>Rare</option>
-            <option value={"Very rare"}>Very rare</option>
-            <option>...</option>
+            <option value={"Very Rare"}>Very Rare</option>
+            <option value={"Legendary"}>Legendary</option>
+            <option value={"Artifact"}>Artifact</option>
+            <option value={"Varies"}>Varies</option>
+            <option value={"nknow Rarit"}>Unknow Rarity</option>
           </select>
         </div>
         <div className="col-6">
@@ -146,13 +109,16 @@ export const FormMagicItems = () => {
             onChange={handleOnChange}
             name="magic_item_type"
             className="form-select"
-            disabled={isDisabled.magic_item_type}
+            disabled={disabledFields.magic_item_type}
           >
             <option value="">---</option>
-            <option value={"Anillo"}>Anillo</option>
-            <option value={"Poción"}>Poción</option>
-            <option value={"Varita"}>Varita</option>
-            <option value={"3"}>...</option>
+            <option value={"Wondrous Item"}>Wondrous Item</option>
+            <option value={"Rod"}>Rod</option>
+            <option value={"Scroll"}>Scroll</option>
+            <option value={"Staff"}>Staff</option>
+            <option value={"Wand"}>Wand</option>
+            <option value={"Ring"}>Ring</option>
+            <option value={"Potion"}>Potion</option>
           </select>
         </div>
         <div className="col-md-4">
@@ -163,11 +129,23 @@ export const FormMagicItems = () => {
             onChange={handleOnChange}
             name="base_armor"
             className="form-select"
-            disabled={isDisabled.base_armor}
+            disabled={disabledFields.base_armor}
           >
             <option value="">---</option>
-            <option value="0">afa</option>
-            <option value="1">acac</option>
+            <option value="Breastplate">Breastplate</option>
+            <option value="Chain Mail">Chain Mail</option>
+            <option value="Chain Shirt">Chain Shirt</option>
+            <option value="Half Plate">Half Plate</option>
+            <option value="Hide">Hide</option>
+            <option value="Leather">Leather</option>
+            <option value="Padded">Padded</option>
+            <option value="Plate">Plate</option>
+            <option value="Ring Mail">Ring Mail</option>
+            <option value="Scale Mail">Scale Mail</option>
+            <option value="Shield">Shield</option>
+            <option value="Spiked Armor">Spiked Armor</option>
+            <option value="Splint">Splint</option>
+            <option value="Studded Leather">Studded Leather</option>
           </select>
         </div>
         <div className="col-md-4">
@@ -178,11 +156,12 @@ export const FormMagicItems = () => {
             onChange={handleOnChange}
             name="dex_bonus"
             className="form-select"
-            disabled={isDisabled.dex_bonus}
+            disabled={disabledFields.dex_bonus}
           >
             <option value="">---</option>
-            <option value="0">afvavf</option>
-            <option value="1">afafa</option>
+            <option value="Full Modifier">Full Modifier</option>
+            <option value="Max 2">Max 2</option>
+            <option value="None">None</option>
           </select>
         </div>
         <div className="col-md-4">
@@ -198,7 +177,7 @@ export const FormMagicItems = () => {
             type="text"
             className="form-control"
             name="str_requirement"
-            disabled={isDisabled.str_requirement}
+            disabled={disabledFields.str_requirement}
           />
         </div>
         <div className="col-md-4">
@@ -209,11 +188,11 @@ export const FormMagicItems = () => {
             onChange={handleOnChange}
             name="stealth_check"
             className="form-select"
-            disabled={isDisabled.stealth_check}
+            disabled={disabledFields.stealth_check}
           >
             <option value="">---</option>
-            <option value={"Ninguno"}>Ninguno</option>
-            <option value={"Desventaja"}>Desventaja</option>
+            <option value={"None"}>None</option>
+            <option value={"Disadvantage"}>Disadvantage</option>
           </select>
         </div>
         <div className="col-md-4">
@@ -224,11 +203,62 @@ export const FormMagicItems = () => {
             onChange={handleOnChange}
             name="base_weapon"
             className="form-select"
-            disabled={isDisabled.base_weapon}
+            disabled={disabledFields.base_weapon}
           >
             <option value="">---</option>
-            <option value={"0"}>dfghd</option>
-            <option value={"1"}>ndndfg</option>
+            <option value="Antimatter Rifle">Antimatter Rifle</option>
+            <option value="Battleaxe">Battleaxe</option>
+            <option value="Blowgun">Blowgun</option>
+            <option value="Boomerang">Boomerang</option>
+            <option value="Club">Club</option>
+            <option value="Crossbow, Hand">Crossbow, Hand</option>
+            <option value="Crossbow, Heavy">Crossbow, Heavy</option>
+            <option value="Crossbow, Light">Crossbow, Light</option>
+            <option value="Dagger">Dagger</option>
+            <option value="Dart">Dart</option>
+            <option value="Double-Bladed Scimitar">
+              Double-Bladed Scimitar
+            </option>
+            <option value="Flail">Flail</option>
+            <option value="Glaive">Glaive</option>
+            <option value="Greataxe">Greataxe</option>
+            <option value="Greatclub">Greatclub</option>
+            <option value="Greatsword">Greatsword</option>
+            <option value="Halberd">Halberd</option>
+            <option value="Handaxe">Handaxe</option>
+            <option value="Javelin">Javelin</option>
+            <option value="Lance">Lance</option>
+            <option value="Laser Pistol">Laser Pistol</option>
+            <option value="Laser Rifle">Laser Rifle</option>
+            <option value="Light Hammer">Light Hammer</option>
+            <option value="Longbow">Longbow</option>
+            <option value="Longsword">Longsword</option>
+            <option value="Mace">Mace</option>
+            <option value="Maul">Maul</option>
+            <option value="Morningstar">Morningstar</option>
+            <option value="Musket">Musket</option>
+            <option value="Net">Net</option>
+            <option value="Pike">Pike</option>
+            <option value="Pistol">Pistol</option>
+            <option value="Pistol, Automatic">Pistol, Automatic</option>
+            <option value="Quarterstaff">Quarterstaff</option>
+            <option value="Rapier">Rapier</option>
+            <option value="Revolver">Revolver</option>
+            <option value="Rifle, Automatic">Rifle, Automatic</option>
+            <option value="Rifle, Hunting">Rifle, Hunting</option>
+            <option value="Scimitar">Scimitar</option>
+            <option value="Semiautomatic Pistol">Semiautomatic Pistol</option>
+            <option value="Shortbow">Shortbow</option>
+            <option value="Shortsword">Shortsword</option>
+            <option value="Shotgun">Shotgun</option>
+            <option value="Sickle">Sickle</option>
+            <option value="Sling">Sling</option>
+            <option value="Spear">Spear</option>
+            <option value="Trident">Trident</option>
+            <option value="War Pick">War Pick</option>
+            <option value="Warhammer">Warhammer</option>
+            <option value="Whip">Whip</option>
+            <option value="Yklwa">Yklwa</option>
           </select>
         </div>
         <div className="col-12">
@@ -254,7 +284,7 @@ export const FormMagicItems = () => {
             className="form-control"
             name="attunement_description"
             placeholder="Enter the magic item attunement description."
-            disabled={isDisabled.attunement_description}
+            disabled={disabledFields.attunement_description}
           />
         </div>
         <div className="col-md-12">
