ccfd53a40faa8706e3802af395244f2584a41842
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-09-23 08:53:06 +0000

    feat: traducir al ingles el navbar desplegable

--- src/front/components/CollectionDropdown.jsx
+++ src/front/components/CollectionDropdown.jsx
@@ -5,43 +5,43 @@ export const CollectionDropdown = ({ closeDropdown }) => {
   const creationsList = [
     {
       to: "user/create-character",
-      text: " Crear personajes",
+      text: " Create Characters",
       styles: styles.createBtn,
     },
     {
       to: "user/create-spell",
-      text: " Crear hechizos",
+      text: " Create Spells",
       styles: styles.createBtn,
     },
     {
       to: "user/create-magic-item",
-      text: " Crear articulos magicos",
+      text: " Create Magics Items",
       styles: styles.createBtn,
     },
-    { to: "", text: " Crear monstruos", styles: styles.createBtn },
+    { to: "", text: " Create Monsters", styles: styles.createBtn },
     {
       to: "user/create-background",
-      text: " Crear transfondo",
+      text: " Create Background",
       styles: styles.createBtn,
     },
     {
       to: "user/create-specie",
-      text: " Crear especie",
+      text: " Create Species",
       styles: styles.createBtn,
     },
     {
       to: "user/create-subclasses",
-      text: " Crear clase",
+      text: " Create Class",
       styles: styles.createBtn,
     },
     {
       to: "user/create-feats",
-      text: " Crear hazaña",
+      text: " Create Feats",
       styles: styles.createBtn,
     },
     {
       to: "user/create-campaign",
-      text: " Crear campaña",
+      text: " Create Campaigns",
       styles: styles.createBtn,
     },
   ];
@@ -53,32 +53,32 @@ export const CollectionDropdown = ({ closeDropdown }) => {
         <section className={styles.dropLeft}>
           <Link to={"/user/characters"}>
             <button className={styles.charactersBtn} onClick={closeDropdown}>
-              <span className={styles.titleBtn}>MIS PERSONAJES</span>
+              <span className={styles.titleBtn}>MY CHARACTER</span>
             </button>
           </Link>
           <Link to={"/user/campaigns"}>
             <button className={styles.campaignsBtn}>
-              <span className={styles.titleBtn}>MIS CAMPAÑAS</span>
+              <span className={styles.titleBtn}>MY CAMPAIGNS</span>
             </button>
           </Link>
           <Link to={"/user/magics-items"}>
             <button className={styles.magicsBtn} onClick={closeDropdown}>
-              <span className={styles.titleBtn}> MIS ARTICULOS MAGICOS </span>
+              <span className={styles.titleBtn}> MY MAGICS ITEMS </span>
             </button>
           </Link>
           <Link to={""}>
             <button className={styles.spellsBtn}>
-              <span className={styles.titleBtn}>MIS HECHIZOS</span>
+              <span className={styles.titleBtn}>MY SPELLS</span>
             </button>
           </Link>
           <Link to={""}>
             <button className={styles.monstersBtn}>
-              <span className={styles.titleBtn}>MIS MONSTRUOS</span>
+              <span className={styles.titleBtn}>MY MONSTERS</span>
             </button>
           </Link>
           <Link to={""}>
             <button className={styles.diceBtn}>
-              <span className={styles.titleBtn}>MIS DADOS</span>
+              <span className={styles.titleBtn}>MY DICE</span>
             </button>
           </Link>
         </section>
@@ -86,7 +86,7 @@ export const CollectionDropdown = ({ closeDropdown }) => {
         {/* **** SECCION DERECHA DEL DROPDOWN **** */}
       <div className={styles.dropRightContainer}>
         <section>
-          <h3 className="text-white mt-2">Creaciones:</h3>
+          <h3 className="text-white mt-2">Homebrew:</h3>
           <div className={styles.dropRight}>
             {creationsList.map((item, index) => (
               <Link
