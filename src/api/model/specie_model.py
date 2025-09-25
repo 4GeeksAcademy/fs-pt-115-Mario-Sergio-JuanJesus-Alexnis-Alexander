edac329bdb82f16c4f49e1193712fe80ca917543
Alexnis-Herrera <alexnisherrerag@gmail.com>
2025-09-25 13:23:13 +0000

    specie

--- src/api/model/specie_model.py
+++ src/api/model/specie_model.py
@@ -5,23 +5,41 @@ from typing import Optional
 
 class Specie(db.Model):
     specie_id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    version: Mapped[Optional[str]]= mapped_column(Text)
+    size: Mapped[str] = mapped_column(String(255), nullable=False)
+    speed_walking: Mapped[Optional[str]]= mapped_column(Text)
+    speed_burrowing: Mapped[Optional[str]]= mapped_column(Text)
+    speed_flying: Mapped[Optional[str]]= mapped_column(Text)
+    speed_swimming: Mapped[Optional[str]]= mapped_column(Text)
     short_description: Mapped[Optional[str]]= mapped_column(Text)
     group: Mapped[Optional[str]]= mapped_column(String(255))
-    description: Mapped[Optional[str]]= mapped_column(Text)
-    trait_desc: Mapped[Optional[str]]= mapped_column(Text)
-    trait: Mapped[Optional[str]]= mapped_column(String(255))
-    avatar: Mapped[Optional[str]]= mapped_column(String(255))
+    description: Mapped[str] = mapped_column(String(255), nullable=False)
+    hide_trait: Mapped[Optional[str]]= mapped_column(Text)
+    specie_trait: Mapped[Optional[str]]= mapped_column(String(255))
+    will_have_species: Mapped[Optional[str]]= mapped_column(String(255))
+    large_avatar: Mapped[Optional[str]]= mapped_column(String(255))
+    portrait_avatar: Mapped[Optional[str]]= mapped_column(String(255))
 
     user_id: Mapped[int]= mapped_column(ForeignKey("user.id"))
 
 def serialize (self):
     return{
         "specie_id": self.specie_id,
+        "name": self.name,
+        "version": self.version,
+        "size": self.size,
+        "speed_walking": self.speed_walking,
+        "speed_burrowing": self.speed_burrowing,
+        "speed_flying": self.speed_flying,
+        "speed_swimming": self.speed_swimming,
         "short_description": self.short_description,
         "group": self.group,
         "description": self.description,
-        "trait_desc": self.trait_desc,
-        "trait": self.trait,
-        "avatar": self.avatar,
+        "hide_trait": self.hide_trait,
+        "specie_trait": self.specie_trait,
+        "will_have_species": self.will_have_species,
+        "large_avatar": self.large_avatar,
+        "portrait_avatar": self.portrait_avatar,
         "user_id": self.user_id
     }
