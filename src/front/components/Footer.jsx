d60711d6a0c5d5d323067b69df8ca59b01a04967
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-09-27 15:52:35 +0000

    feat: crear un footer a falta de ver si añadimos politicas, cookies...

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,11 +1,73 @@
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+import styles from '../styles/components/footer.module.css'
+
+export const Footer = () => {
+
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.footerContainer}>
+				<div className={styles.footerContent}>
+
+					{/* Company Info Section */}
+					<div className={styles.footerSection}>
+						<h3>Acerca de Nosotros</h3>
+						<p>
+							Somos un grupo comprometido con ofrecer los mejores servicios a la comunidad D&D.
+							Gracias a tantos años jugando queremos ofrecer un mejor servicio online para ayudar
+							en todo lo posible a la comunidad a poder controlar mejor sus partidas.
+
+						</p>
+					</div>
+
+					<div className={styles.footerSection}>
+						<h3>Enlaces Rápidos</h3>
+						<ul>
+							<li><a href="#">Inicio</a></li>
+							<li><a href="#">Servicios</a></li>
+							<li><a href="#">Productos</a></li>
+						</ul>
+					</div>
+
+					<div className={styles.footerSection}>
+						<h3>Contacto</h3>
+						<div className={styles.contactInfo}>
+							<div className={styles.contactItem}>
+								<svg viewBox="0 0 24 24">
+									<path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
+								</svg>
+								<span>Calle Principal 123, Ciudad</span>
+							</div>
+							<div className={styles.contactItem}>
+								<svg viewBox="0 0 24 24">
+									<path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
+								</svg>
+								<span>+34 123 456 789</span>
+							</div>
+							<div className={styles.contactItem}>
+								<svg viewBox="0 0 24 24">
+									<path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
+								</svg>
+								<a href=''>team.masterofinfinity@gmail.com</a>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+
+				<div className={styles.footerBottom}>
+					<div className={styles.footerBottomContent}>
+						<div className={styles.copyright}>
+							<p>&copy; 2025 MAsterOfInfinity. Todos los derechos reservados.</p>
+						</div>
+						<nav className={styles.footerNav}>
+							<a href="#">Política de Privacidad</a>
+							<a href="#">Términos de Uso</a>
+							<a href="#">Cookies</a>
+							<a href="#">Sitemap</a>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
--- src/front/layout/Root.layout.jsx
+++ src/front/layout/Root.layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
 
 export const RootLayout = () => {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </>
   );
 };
