99ca0446d3fad67341e0a3c782b8342cbdf72d0e
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-09-28 17:53:28 +0000

    feat: seguir añadiendo funcionalidades al footer y añadir las cookies para la primera vez que entres

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -46,7 +46,7 @@ export const Footer = () => {
 								<svg viewBox="0 0 24 24">
 									<path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
 								</svg>
-								<a href=''>team.masterofinfinity@gmail.com</a>
+								<a href='mailto:team.masterofinfinity@gmail.com'>team.masterofinfinity@gmail.com</a>
 							</div>
 							
 						</div>
@@ -60,8 +60,6 @@ export const Footer = () => {
 						</div>
 						<nav className={styles.footerNav}>
 							<a href="#">Política de Privacidad</a>
-							<a href="#">Términos de Uso</a>
-							<a href="#">Cookies</a>
 							<a href="#">Sitemap</a>
 						</nav>
 					</div>
--- src/front/layout/Root.layout.jsx
+++ src/front/layout/Root.layout.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
+import { Cookies } from "../components/Cookies";
 
 export const RootLayout = () => {
   return (
     <>
       <Navbar />
+      <Cookies/>
       <main>
         <Outlet />
       </main>
       <Footer />
+      
     </>
   );
 };
